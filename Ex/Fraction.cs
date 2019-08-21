aa275a719b492571a395ca56349f8619316c25f7
Федина Юлия <yu.belova@2gis.ru>
2019-08-21 16:28:34 +0700

    Задание для 13 лекции

---
+++ Ex/Fraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex
+{
+    [Serializable]
+    public class Fraction
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Fraction(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return X == 0 ? $"{X}" : $"{X}/{Y}";
+        }
+    }
+}
